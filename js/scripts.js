df9531596a9218cb77125b9853850d86246a8a08
ceribelimello <30481748+ceribelimello@users.noreply.github.com>
2019-09-06 10:24:37 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 var n = 1;
 
-
+switch(n) {
+  case 1:
+    console.log("azul");
+    break;
+  case 2:
+    console.log("amarelo");
+    break;
+  case 3:
+    console.log("verde");
+    break;
+  case 4:
+    console.log("cinza");
+    break;
+  case 5:
+    console.log("vermelho");
+    break;
+  default:
+    console.log("roxo");
+  }
 
 window.setInterval(function(){ 
   n++;
@@ -8,9 +26,7 @@ window.setInterval(function(){
   	n = 1;
   };
   console.log(n);
-}, 5000);
 
-jQuery(n).change(function() {
   switch(n) {
   case 1:
     console.log("azul");
@@ -30,5 +46,6 @@ jQuery(n).change(function() {
   default:
     console.log("roxo");
   }
-});
+
+}, 5000);
 

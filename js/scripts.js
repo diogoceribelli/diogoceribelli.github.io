79cef3a94d2465c1c79c46d892bbfdaba0774fcb
ceribelimello <30481748+ceribelimello@users.noreply.github.com>
2019-09-06 10:08:26 -0300

    att script

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,14 @@
 var n = 1;
 
-switch(n) {
-  case 1:
-    console.log("azul");
-    break;
-  case 2:
-    console.log("amarelo");
-    break;
-  case 3:
-    console.log("verde");
-    break;
-  case 4:
-    console.log("cinza");
-    break;
-  case 5:
-    console.log("vermelho");
-    break;
-  default:
-    console.log("roxo");
-}
-
 window.setInterval(function(){ 
   n++;
   if (n > 5) {
   	n = 1;
   };
   console.log(n);
+}, 5000);
 
+jQuery(n).change(function() {
   switch(n) {
   case 1:
     console.log("azul");
@@ -46,6 +28,5 @@ window.setInterval(function(){
   default:
     console.log("roxo");
   }
-
-}, 5000);
+});
 

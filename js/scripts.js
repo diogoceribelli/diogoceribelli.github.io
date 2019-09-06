1a26c335b0aab72897b5493cbd6b27e379c7d709
ceribelimello <30481748+ceribelimello@users.noreply.github.com>
2019-09-06 10:20:09 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,33 +11,33 @@ window.setInterval(function(){
 jQuery(n).change(function() {
   switch(n) {
   case 1:
-    jQuery("body").removeClass();
-    jQuery("body").addClass("blue");
+    /*jQuery("body").removeClass();
+    jQuery("body").addClass("blue");*/
     console.log(n + "foi");
     break;
   case 2:
-    jQuery("body").removeClass();
-    jQuery("body").addClass("yellow");
+    /*jQuery("body").removeClass();
+    jQuery("body").addClass("yellow");*/
     console.log(n + "foi");
     break;
   case 3:
-    jQuery("body").removeClass();
-    jQuery("body").addClass("green");
+    /*jQuery("body").removeClass();
+    jQuery("body").addClass("green")*/;
     console.log(n + "foi");
     break;
   case 4:
-    jQuery("body").removeClass();
-    jQuery("body").addClass("grey");
+    /*jQuery("body").removeClass();
+    jQuery("body").addClass("grey");*/
     console.log(n + "foi");
     break;
   case 5:
-    jQuery("body").removeClass();
-    jQuery("body").addClass("red");
+    /*jQuery("body").removeClass();
+    jQuery("body").addClass("red");*/
     console.log(n + "foi");
     break;
   default:
-    jQuery("body").removeClass();
-    jQuery("body").addClass("purple");
+    /*jQuery("body").removeClass();
+    jQuery("body").addClass("purple");*/
     console.log(n + "foi");
   }
 });

4eecdc222e5d17ba786d8aa6b2d1c32fff701051
ceribelimello <30481748+ceribelimello@users.noreply.github.com>
2019-09-06 10:13:52 -0300

    trying again on colors

--- js/scripts.js
+++ js/scripts.js
@@ -11,34 +11,22 @@ window.setInterval(function(){
 jQuery(n).change(function() {
   switch(n) {
   case 1:
-    jQuery("body").css({
-    	'background-color':'blue'
-    });
+    jQuery("body").css("background-color","blue");
     break;
   case 2:
-    jQuery("body").css({
-    	'background-color':'yellow'
-    });
+    jQuery("body").css("background-color","yellow");
     break;
   case 3:
-    jQuery("body").css({
-    	'background-color':'green'
-    });
+    jQuery("body").css("background-color","green");
     break;
   case 4:
-    jQuery("body").css({
-    	'background-color':'grey'
-    });
+    jQuery("body").css("background-color","grey");
     break;
   case 5:
-    jQuery("body").css({
-    	'background-color':'red'
-    });
+    jQuery("body").css("background-color","red");
     break;
   default:
-    jQuery("body").css({
-    	'background-color':'purple'
-    });
+    jQuery("body").css("background-color","purple");
   }
 });
 

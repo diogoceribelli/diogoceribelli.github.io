ed2131abf9bef6d7187ad977d53be1804740e080
ceribelimello <30481748+ceribelimello@users.noreply.github.com>
2019-09-06 10:04:28 -0300

    uptade script cores

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 var n = 1;
-window.setInterval(function(){ 
-  n++;
-  if (n > 5) {
-  	n = 1;
-  };
-  console.log(n);
-}, 5000);
 
 switch(n) {
   case 1:
@@ -26,3 +19,33 @@ switch(n) {
   default:
     console.log("roxo");
 }
+
+window.setInterval(function(){ 
+  n++;
+  if (n > 5) {
+  	n = 1;
+  };
+  console.log(n);
+
+  switch(n) {
+  case 1:
+    console.log("azul");
+    break;
+  case 2:
+    console.log("amarelo");
+    break;
+  case 3:
+    console.log("verde");
+    break;
+  case 4:
+    console.log("cinza");
+    break;
+  case 5:
+    console.log("vermelho");
+    break;
+  default:
+    console.log("roxo");
+  }
+
+}, 5000);
+
